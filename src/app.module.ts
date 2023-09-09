cda0d205fbc928d9b0f138bd061eb7b7c786249c
Mekkawy <ahmed.mekawy@speakol.com>
2023-09-09 17:44:49 +0300

    add request logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VoucherCodeModule } from './voucher_code/voucherCodeModule';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [VoucherCodeModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,17 @@
+import { Injectable, NestMiddleware, RequestMethod } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log(`req:`, {
+      headers: req.headers,
+      body: req.body,
+      originalUrl: req.originalUrl,
+    });
+
+    if (next) {
+      next();
+    }
+  }
+}
